37772135d227870b7cee00c75fe04f8f54249c1a
Chirag <chiragsinghaug10@gmail.com>
2025-04-06 21:31:39 +0530

    Latest Code

--- models/Payment-Model.js
+++ models/Payment-Model.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const PaymentSchema = new mongoose.Schema({
-    orderId: { type: mongoose.Schema.Types.ObjectId, ref: 'Order', required: true }, // Reference to an Order
+    orderId: { type: mongoose.Schema.Types.ObjectId, ref: 'Order', required: true },
     paymentMethod: { type: String, required: true, enum: ['Credit Card', 'Debit Card', 'PayPal', 'Cash on Delivery'] },
     paymentStatus: { type: String, required: true, enum: ['Pending', 'Completed', 'Failed', 'Cancelled'], default: 'Pending' },
     amount: { type: Number, required: true, min: [0, 'Amount must be greater than 0'] },
